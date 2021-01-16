29f66616252e0e8b98cebc327079198951f1430e
Ajeet Singh <imajeet5@gmail.com>
2021-01-16 13:14:39 +0530

    Code clean up

--- src/index.js
+++ src/index.js
@@ -23,7 +23,6 @@ new Phaser.Game(config);
 let bird = null;
 let totalDelta = null;
 
-// Loading assets, such as images, music, animation
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', '/assets/bird.png');
@@ -31,22 +30,10 @@ function preload() {
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
-  // sprint is an game object that has more properties that we can play with
+
   bird = this.physics.add
     .sprite(config.width * 0.1, config.height / 2, 'bird')
     .setOrigin(0);
-
-  // bird.body.gravity.y = 200;
-
-  // console.log(bird.body);
-  // debugger;
 }
 
-function update(time, delta) {
-  if (totalDelta >= 1000) {
-    // console.log(totalDelta);
-    console.log(bird.body.velocity.y);
-    totalDelta = 0;
-  }
-  totalDelta += delta;
-}
+function update(time, delta) {}
