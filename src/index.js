5cf7d464d6ad287f2ce5be7e603d7316d1b9ec2c
Ajeet Singh <imajeet5@gmail.com>
2021-01-16 18:15:57 +0530

    group pipes

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const config = {
   physics: {
     default: 'arcade',
     arcade: {
-      gravity: { y: 200 },
+      // gravity: { y: 400 },
+      debug: true,
     },
   },
   scene: {
@@ -20,20 +21,79 @@ const config = {
 
 new Phaser.Game(config);
 
+const VELOCITY = 200;
+const PIPES_TO_RENDER = 4;
+const flapVelocity = 250;
+const initialBirdPosition = {
+  x: config.width * 0.1,
+  y: config.height / 2,
+};
+
 let bird = null;
-let totalDelta = null;
+
+let pipes = null;
+let pipeHorizontalDistance = 0;
+const pipeVerticalDistanceRange = [150, 250];
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
-  this.load.image('bird', '/assets/bird.png');
+  this.load.image('bird', '/assets/BirdPng/Frame-1.png');
+  this.load.image('pipe', './assets/pipe.png');
 }
 
 function create() {
   this.add.image(0, 0, 'sky').setOrigin(0);
 
   bird = this.physics.add
-    .sprite(config.width * 0.1, config.height / 2, 'bird')
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, 'bird')
     .setOrigin(0);
+  bird.body.gravity.y = 400;
+  bird.setScale(0.07);
+  
+  pipes = this.physics.add.group()
+
+  for (let i = 0; i < PIPES_TO_RENDER; i++) {
+    const upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    const lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+
+    placePipe(upperPipe, lowerPipe);
+  }
+  
+  pipes.setVelocityX(-200);
+
+  this.input.on('pointerdown', flap);
+  this.input.keyboard.on('keydown_SPACE', flap);
 }
 
-function update(time, delta) {}
+function update(time, delta) {
+  if (bird.y > config.height || bird.y < -bird.height) {
+    restartBirdPosition();
+  }
+}
+
+function placePipe(uPipe, lPipe) {
+  pipeHorizontalDistance += 400;
+  let pipeVerticalDistance = Phaser.Math.Between(...pipeVerticalDistanceRange);
+  let pipeVerticalPosition = Phaser.Math.Between(
+    0 + 20,
+    config.height - 20 - pipeVerticalDistance
+  );
+
+  uPipe.x = pipeHorizontalDistance;
+  uPipe.y = pipeVerticalPosition;
+
+  lPipe.x = uPipe.x;
+  lPipe.y = uPipe.y + pipeVerticalDistance;
+
+ 
+}
+
+function restartBirdPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
+}
+
+function flap() {
+  bird.body.velocity.y = -flapVelocity;
+}
